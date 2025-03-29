74486bc398a8c4e52a114e0e781543bcdd4453ed
Haimanotbb <167545658+Haimanotbb@users.noreply.github.com>
2025-03-28 22:44:11 -0400

    Update admin.py

--- pickup_sports/scheduler/admin.py
+++ pickup_sports/scheduler/admin.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Sport, Game, Participant
 
-# Register your CustomUser model with the Django admin
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    # Optionally customize the admin fields, list display, etc.
     pass
 
 # Register other models
